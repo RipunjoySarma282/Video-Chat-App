b7555801192bbe5d33e20a15d1600717e2583aaf
Ripunjoy <ripunjoysarma2000@gmail.com>
2021-07-12 01:28:43 +0530

    expires is define in the file itself

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -38,7 +38,7 @@ const handleErrors=(err)=>{
 
 
 // create jwt token
-const maxAge = process.env.JWT_EXPIRES_IN;
+const maxAge = 2 * 24 * 60 * 60;
 const createToken = (id) =>{
     return jwt.sign({id},process.env.SECRET_KEY,{
         expiresIn: maxAge
